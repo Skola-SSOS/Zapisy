abb71be6ea3ffc67d8dd8121381499ad9b505fac
LordMefloun <71554044+LordMefloun@users.noreply.github.com>
2021-11-02 18:09:46 +0100

    Nove zapisy

--- ZPR/pozditani.c
+++
@@ -1,2 +0,0 @@
-#include <stdio.h>
-int main (){float hodnota;printf("Zadej hodnotu v cm: ");scanf("%f", &hodnota);printf("mm %f\ncm %f\ndm %f\nm %f\nkm %f", hodnota * 10, hodnota, hodnota/10, hodnota/100, hodnota/1000000);return 0;}
